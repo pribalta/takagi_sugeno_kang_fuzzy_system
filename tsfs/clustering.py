d78a0c967eaf16df02fb199d4bd34248b85c32e9
Pablo Ribalta Lorenzo <pribalta@nvidia.com>
2020-09-27 11:42:56 +0200

    Started work in cmeans
    Signed-off-by: Pablo Ribalta Lorenzo <pribalta@nvidia.com>

--- tsfs/clustering.py
+++ tsfs/clustering.py
@@ -13,3 +13,19 @@ class Cluster(ABC):
         :return: None
         """
         return NotImplementedError
+
+
+class CMeansClustering(Cluster):
+    def __init__(self, n_clusters: int, error: float = 1e-4, max_iters: int = 200):
+        """
+        Constructor of C-Means Clustering
+        :param n_clusters: (int) Number of clusters
+        :param error: (float) Stop condition
+        :param max_iters: (int) Total iteration number
+        """
+        self._n_clusters = n_clusters
+        self._error = error
+        self._max_iters = max_iters
+
+    def fit(self, x: np.ndarray, y: np.ndarray) -> None:
+        pass
