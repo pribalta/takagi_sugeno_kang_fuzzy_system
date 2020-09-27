837b876b053dbe8c6ee83dda10da778faf2f8f12
Pablo Ribalta Lorenzo <pribalta@nvidia.com>
2020-09-27 11:50:37 +0200

    Work in process for cmeans. Begin working on update function
    Signed-off-by: Pablo Ribalta Lorenzo <pribalta@nvidia.com>

--- tsfs/clustering.py
+++ tsfs/clustering.py
@@ -1,11 +1,12 @@
 from abc import ABC, abstractmethod
+from typing import Any
 
 import numpy as np
 
 
 class Cluster(ABC):
     @abstractmethod
-    def fit(self, x: np.ndarray, y: np.ndarray) -> None:
+    def fit(self, x: np.ndarray, y: np.ndarray) -> Any:
         """
         Fit input to distribution
         :param x: (np.ndarray) Input values
@@ -27,5 +28,42 @@ class CMeansClustering(Cluster):
         self._error = error
         self._max_iters = max_iters
 
-    def fit(self, x: np.ndarray, y: np.ndarray) -> None:
-        pass
+        self._fuzzy_index = None
+        self._fitted = None
+        self._center = None
+        self._train_u = None
+        self._variance = None
+        self._losses = None
+        self._dimensions = None
+
+    def fit(self, x: np.ndarray, y: np.ndarray) -> Any:
+        if min(x.shape[0], x.shape[1] - 1) >= 3:
+            self._fuzzy_index = min(x.shape[0], x.shape[1] - 1) / (min(x.shape[0], x.shape[1] - 1) - 2)
+        else:
+            self._fuzzy_index = 2
+
+        n = x.shape[0]
+        self._dimensions = x.shape[1]
+
+        u = np.random.rand(self._n_clusters, n)
+
+        self._losses = []
+        for t in range(self._max_iters):
+            u, v, loss, signal = self._update(x, u)
+            self._losses.append(loss)
+            print('[FCM Iter {}] Loss: {:.4f}'.format(t, loss))
+            if signal:
+                break
+        self._fitted = True
+        self._center = v
+        self._train_u = u
+        self._variance = np.zeros(self._center.shape)
+
+        for i in range(self._dimensions):
+            self._variance[:, i] = np.sum(
+                u * ((x[:, i][:, np.newaxis] - self._center[:, i].transpose()) ** 2).T, axis=1
+            ) / np.sum(u, axis=1)
+
+        self._variance = np.fmax(self._variance, np.finfo(np.float64).eps)
+
+        return self
